9f07e9a5119cf50760c7d30178091e1d0a5c6189
theanishtar <dangtt135@gmail.com>
2024-03-07 16:08:43 +0700

    Update badword.route.js

--- app/routes/badword.route.js
+++ app/routes/badword.route.js
@@ -11,10 +11,4 @@ module.exports = function (app, redis) {
     );
     next();
   });
-
-  app.get('/api/badwords', auth.isAdmin, (req, res) => controller.getAllBadwords(req, res, redis));
-  app.get('/api/badword', (req, res) => controller.getBadwordByName(req, res, redis)); //api/badword?name=cút
-  app.post('/api/badwords', (req, res) => controller.checkBadword(req, res, redis));
-  app.post('/api/cleanwords', (req, res) => controller.cleanWords(req, res, redis));
-  app.get('/api/cleanwords', (req, res) => controller.cleanWords(req, res, redis)); //api/cleanword?word=cút
 };
